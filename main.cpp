c133979003705c4806a80061f753d8d9cf27486d
Paulo Miranda <paulo.miranda@utec.edu.pe>
2025-07-03 23:36:44 -0500

    agregando archivos innecesarios

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "main_auxiliar.h"
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
---
+++ main_auxiliar.h
@@ -0,0 +1,8 @@
+//
+// Created by paulo on 3/07/2025.
+//
+
+#ifndef ROZ_H
+#define ROZ_H
+
+#endif //ROZ_H
