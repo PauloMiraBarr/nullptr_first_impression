2cbf03356d68ef7d7f1085d50a906cd0de95427c
Paulo Miranda <paulo.miranda@utec.edu.pe>
2025-07-04 16:56:36 -0500

    limpiando un poco

--- TextLoaderTest.cpp
+++ TextLoaderTest.cpp
@@ -29,22 +29,6 @@ void show_map_of_first_sample() {
     } std::cout << "\n";
 }
 
-// not required to test
-void show_data() {
-    const auto& dataset = loader.get_dataset();
-    // Mostrar los primeros ejemplos (puedes imprimir 3 como prueba)
-    for (size_t i = 0; i < 3 && i < dataset.size(); ++i) {
-        std::cout << "[" << i + 1 << "] ";
-        std::cout << "Etiqueta: " << dataset[i].label << ", Vector: ";
-
-        // Mostrar primeros 10 valores del vector para no saturar la pantalla
-        for (size_t j = 0; j < 10 && j < dataset[i].vectorized_text.size(); ++j) {
-            std::cout << dataset[i].vectorized_text[j] << " ";
-        }
-        std::cout << "..." << std::endl;
-    }
-}
-
 
 int main() {
     load_data();
